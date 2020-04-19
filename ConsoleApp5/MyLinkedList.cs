cb81144ffffdf5c8bb2c53269a7019057752df19
Anna <salatik339@mail.ru>
2020-04-19 13:34:11 +0300

    Add project files.

---
+++ ConsoleApp5/MyLinkedList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class MyLinkedList<T> : IEnumerable<T>
+    {
+        Node<T> head; 
+        Node<T> tail; 
+        int count;  
+
+        // добвление в начало
+        public void AddHead(T data) 
+        {
+            Node<T> node = new Node<T>(data);
+            node.Next = head;
+            head = node;
+            if (count == 0)
+                tail = head;
+            count++;
+        }
+
+        // удаление элемента из начала
+        public bool RemoveHead(T data)
+        {
+            head = head.Next;
+            // если после удаления список пуст, сбрасываем tail
+            if (head == null)
+            tail = null;          
+            return true; 
+        }
+
+        // получение значения элемента в начале списка
+        public T GetHead()
+        {
+            return head.Data;
+        }
+
+        // получение значения элемента в начале списка
+        public T GetTail()
+        {
+            return tail.Data;
+        }
+
+        // реализация интерфейса IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList<int> myListOfInt = new MyLinkedList<int>();
+            
+            myListOfInt.AddHead(5);
+            myListOfInt.AddHead(4);
+            myListOfInt.AddHead(3);
+            myListOfInt.AddHead(2);
+            myListOfInt.AddHead(1);
+            foreach (int num in myListOfInt) {
+                Console.WriteLine(num);
+            }
+
+            MyLinkedList<string> myListOfString = new MyLinkedList<string>();
+            myListOfString.AddHead("d");
+            myListOfString.AddHead("c");
+            myListOfString.AddHead("b");
+            myListOfString.AddHead("a");
+            foreach (string str in myListOfString)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
